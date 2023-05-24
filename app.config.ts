d1ac9d1c2ccc7e26c037cdd1bcb625aa516e6526
wangke <wangke@modongli.com>
2023-05-24 11:07:26 +0800

    feat: first commit

---
+++ app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  // Configure Content Wind here
+  cover: '/cover.jpg',
+  socials: {
+    twitter: '',
+    github: 'map-c'
+  },
+  prose: {
+    h1: {
+      icon: 'carbon:sailboat-coastal'
+    }
+  }
+})
